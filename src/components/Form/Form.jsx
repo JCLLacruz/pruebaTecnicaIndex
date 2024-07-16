97fd9ed84aeb7e72408d2ec9ed67320c40cd0c1f
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-07-16 18:47:53 +0200

    last fixes

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,35 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './Form.scss';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { alertIcon } from '../../assets/icons/icons';
 
 const Form = ({ header, inputs }) => {
-	const initialValuesFormik = inputs.reduce((acc, input) => {
-		acc[input.key] = '';
-		return acc;
-	}, {});
-
-	const initialValuesState = inputs.reduce((acc, input) => {
-		acc[input.key] = { isActive: false };
-		return acc;
-	}, {});
-
-	const initialValuesSchema = inputs.reduce((acc, input) => {
-		if (input.validationSchema) {
-			acc[input.key] = input.validationSchema;
-		}
-		return acc;
-	}, {});
-
+	const initialValuesRef = useRef({});
+	const initialSchemaRef = useRef({});
 	const [inputsState, setInputsState] = useState({});
 	const [hidden, setHidden] = useState(true);
 	const [modalContent, setModalContent] = useState({});
 
-	console.log('hidden', hidden);
-
 	useEffect(() => {
-		setInputsState(initialValuesState);
+		initialValuesRef.current = inputs.reduce((acc, input) => {
+			acc[input.key] = '';
+			return acc;
+		}, {});
+
+		initialSchemaRef.current = inputs.reduce((acc, input) => {
+			if (input.validationSchema) {
+				acc[input.key] = input.validationSchema;
+			}
+			return acc;
+		}, {});
+
+		setInputsState(
+			inputs.reduce((acc, input) => {
+				acc[input.key] = { isActive: false };
+				return acc;
+			}, {})
+		);
 	}, [inputs]);
 
 	const handleFocus = (name) => {
@@ -47,93 +47,95 @@ const Form = ({ header, inputs }) => {
 	};
 
 	const shouldShowPlaceholder = (name) => {
-		return !inputsState[name]?.isActive && !formik.values[name];
-	};
+		return !inputsState[name]?.isActive && !formik.values[name] && !formik.errors[name];
+	  };
 
-	const closeModal = () => {
+	  const closeModal = () => {
 		setHidden(true);
-	};
+	  };
 
 	const formik = useFormik({
-		initialValues: initialValuesFormik,
-		validationSchema: Yup.object().shape(initialValuesSchema),
+		initialValues: initialValuesRef.current,
+		validationSchema: Yup.object().shape(initialSchemaRef.current),
 		onSubmit: (values) => {
-			console.log('values', values);
-			setModalContent(values);
-			setHidden(false);
-			formik.resetForm();
+		  console.log('values', values);
+		  setModalContent(values);
+		  setHidden(false);
+		  formik.resetForm({
+			values: initialValuesRef.current,
+		  });
 		},
-	});
+	  });
 
 	return (
 		<div className='container'>
 			<div className='formDiv'>
-			<form className='userForm' onSubmit={formik.handleSubmit}>
-				<h1 className='userFormH1'>{header}</h1>
-				{inputs.map((input) => (
-					<div
-						key={input.key}
-						className={`inputGroup ${inputsState[input.key]?.isActive ? 'active' : ''} ${
-							formik.errors[input.key] && formik.touched[input.key] ? 'error' : ''
-						}`}
-					>
-						<label htmlFor={input.key}>{input.label}</label>
-						<div className='inputBox'>
-							{input.type === 'select' ? (
-								<select
-									id={input.key}
-									name={input.key}
-									value={formik.values[input.key]}
-									onFocus={() => handleFocus(input.key)}
-									onChange={formik.handleChange}
-									onBlur={() => {
-										handleBlur(input.key);
-										formik.handleBlur(input.key);
-									}}
-									onKeyUp={formik.handleChange}
-									className='select'
-									placeholder={input.placeholder}
-								>
-									<option value='' disabled hidden>
-										{input.placeholder}
-									</option>
-									{input.options.map((option) => (
-										<option key={option} value={option}>
-											{option}
+				<form className='userForm' onSubmit={formik.handleSubmit}>
+					<h1 className='userFormH1'>{header}</h1>
+					{inputs.map((input) => (
+						<div
+							key={input.key}
+							className={`inputGroup ${inputsState[input.key]?.isActive ? 'active' : ''} ${
+								formik.errors[input.key] && formik.touched[input.key] ? 'error' : ''
+							}`}
+						>
+							<label htmlFor={input.key}>{input.label}</label>
+							<div className='inputBox'>
+								{input.type === 'select' ? (
+									<select
+										id={input.key}
+										name={input.key}
+										value={formik.values[input.key]}
+										onFocus={() => handleFocus(input.key)}
+										onChange={formik.handleChange}
+										onBlur={() => {
+											handleBlur(input.key);
+											formik.handleBlur(input.key);
+										}}
+										onKeyUp={formik.handleChange}
+										className='select'
+										placeholder={input.placeholder}
+									>
+										<option value='' disabled hidden>
+											{input.placeholder}
 										</option>
-									))}
-								</select>
-							) : (
-								<input
-									className='input'
-									id={input.key}
-									name={input.key}
-									value={formik.values[input.key]}
-									onFocus={() => handleFocus(input.key)}
-									onBlur={() => {
-										handleBlur(input.key);
-										formik.handleBlur(input.key);
-									}}
-									onChange={formik.handleChange}
-									type={input.type}
-								/>
-							)}
-							{formik.errors[input.key] && formik.touched[input.key] && (
-								<div>
-									<div className='alertIcon'>{alertIcon}</div>
-									<div className='hintErrorDiv error'>{formik.errors[input.key]}</div>
-								</div>
-							)}
-							{!formik.errors[input.key] && shouldShowPlaceholder(input.key) && <div className='hintErrorDiv placeholder'>{input.placeholder}</div>}
+										{input.options.map((option) => (
+											<option key={option} value={option}>
+												{option}
+											</option>
+										))}
+									</select>
+								) : (
+									<input
+										className='input'
+										id={input.key}
+										name={input.key}
+										value={formik.values[input.key]}
+										onFocus={() => handleFocus(input.key)}
+										onBlur={() => {
+											handleBlur(input.key);
+											formik.handleBlur(input.key);
+										}}
+										onChange={formik.handleChange}
+										type={input.type}
+									/>
+								)}
+								{formik.errors[input.key] && formik.touched[input.key] && (
+									<div>
+										<div className='alertIcon'>{alertIcon}</div>
+										<div className='hintErrorDiv error'>{formik.errors[input.key]}</div>
+									</div>
+								)}
+								{!formik.errors[input.key] && shouldShowPlaceholder(input.key) && <div className='hintErrorDiv placeholder'>{input.placeholder}</div>}
+							</div>
 						</div>
-					</div>
-				))}
-				<input type='submit' className='submit' value='Submit' />
-			</form>
+					))}
+					<input type='submit' className='submit' value='Submit' />
+				</form>
 			</div>
 			<div className={hidden ? 'containerModal hidden' : 'containerModal'}>
 				<div className='buttonDiv'>
-				<button onClick={closeModal}>Close</button>
+					<button onClick={closeModal}>Close</button>
 				</div>
 				<div className='modal'>
 					<div className='modalContent'>
